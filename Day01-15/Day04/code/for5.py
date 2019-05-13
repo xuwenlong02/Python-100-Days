9ffc942c1ffb2b87ce028a43574d7b578713de3b
Wenlong Xu <myxuan475@126.com>
2019-05-13 21:45:24 +0800

    notes
    Signed-off-by: Wenlong Xu <myxuan475@126.com>

--- Day01-15/Day04/code/for5.py
+++ Day01-15/Day04/code/for5.py
@@ -8,10 +8,12 @@ Date: 2018-03-01
 
 x = int(input('x = '))
 y = int(input('y = '))
-if x > y:
-    (x, y) = (y, x)
-for factor in range(x, 0, -1):
-    if x % factor == 0 and y % factor == 0:
-        print('%d和%d的最大公约数是%d' % (x, y, factor))
-        print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
-        break
+a = x
+b = y
+while a != b:
+    if a > b:
+        a = a-b
+    else:
+        b = b - a
+print('%d和%d的最大公约数是%d' % (x, y, a))
+print('%d和%d的最小公倍数是%d' % (x, y, x * y // a))
